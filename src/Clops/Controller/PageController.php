3e7236854d5f96039314a8c6fe5c16b451eb27de
Alexey Kulikov <a.kulikov@gmail.com>
2014-08-23 12:36:20 +0200

    Waypoint Navigation works and looks just AWESOME

--- src/Clops/Controller/PageController.php
+++ src/Clops/Controller/PageController.php
@@ -25,9 +25,25 @@ namespace Clops\Controller;
          */
         public function indexAction(Request $request, Application $app)
         {
+	        //optional limit for infinite waypoint scrolling
+	        $limit   = $request->get('limit');
+	        if(!$limit){ //current time is the default
+		        $limit = date('Y-m-d H:i:s');
+	        }
+
 	        //get the last 10 commits
-	        $commits = $app['db']->fetchAll("SELECT * FROM commits ORDER BY created DESC LIMIT 10");
-            return $app['twig']->render('index.html.twig', array('commits' => $commits));
+	        $commits = $app['db']->fetchAll("SELECT * FROM commits WHERE created < ? ORDER BY created DESC LIMIT 10", array(
+		        (string)$limit
+	        ));
+
+	        $total   = $app['db']->fetchAssoc("SELECT count(*) AS total FROM commits WHERE created < ?", array(
+		        (string)$limit
+	        ));
+
+            return $app['twig']->render('index.html.twig', array(
+	            'commits' => $commits,
+                'total'   => $total['total']
+            ));
         }
 
     }
